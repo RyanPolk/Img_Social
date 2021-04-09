3f6e4f074750cc19f740c17f9b3943dd4d11060d
Ryan Polkiewicz <45237843+RyanPolk@users.noreply.github.com>
2021-04-09 15:13:01 -0400

    Content changes"

--- Frontend/ContentGet.php
+++ Frontend/ContentGet.php
@@ -2,7 +2,7 @@
     header("Content-Type: text/plain");
     
     //User credentials
-    $sUsername = $_GET["username"];
+    $sPostID = $_GET["postID"];
     
     //variable to hold User info
     $sInfo = "";
@@ -14,7 +14,7 @@
     $dbname = "img_social";
 
     //create the SQL query string, uses BINARY to check for case in fields
-    $sql = "Select username, firstName, registered, profileImg from users where BINARY username='".$sUsername."'";
+    $sql = "SELECT postImg, description, posted, username FROM posts INNER JOIN users ON posts.userID = users.userID WHERE postID=".$sPostID.";";
               
     $conn = new mysqli($servername, $username, $password, $dbname);
     if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
@@ -22,7 +22,7 @@
 
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            $sInfo = $row['username'].";".$row['firstName'].";".$row['registered'].";";
+            $sInfo = $row['description'].";".$row['username'].";".$row['posted'].";".$row['postImg'].";";
         }
     }
     $conn->close();
